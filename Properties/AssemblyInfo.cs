d1e00efeffc5ffea137e8e034f9167c83a2a7f17
Goanny <huchiyama800@gmail.com>
2022-08-11 23:08:22 -0500

    hola

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general acerca de un ensamblado se controla a través del siguiente 
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información 
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Reporte")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Reporte")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Al establecer ComVisible en false, se consigue que los tipos de este ensamblado no sean visibles 
+// para los componentes COM. Si tiene que acceder a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en ese tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el ID. del typelib, si este proyecto se expone en COM
+[assembly: Guid("6be01d4f-281b-4135-9ab2-2fd95b043249")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
